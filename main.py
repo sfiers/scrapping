6d780cab25ab18fc4fc5c0f4203dbe3c52464791
sfiers <simon.fiers@outlook.com>
2021-11-29 18:10:33 +0100

    extract src of img

--- main.py
+++ main.py
@@ -16,5 +16,10 @@ liste_div_centre = soup.find_all("div", class_="centre")
 paragraphe_description = liste_div_centre[1].find("p", class_="description")
 text_description = paragraphe_description.text
 
+div_info = soup.find('div', class_="info")
+img_info = div_info.find("img")
+
+
 print ("Titre de la page:", text_titre)
 print ("La description:", text_description)
+print ("La src de l'img:", img_info["src"])
