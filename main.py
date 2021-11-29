492c85b30db0c79680b60f0abdd32732d5f5d20e
sfiers <simon.fiers@outlook.com>
2021-11-29 18:06:10 +0100

    extract description

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ soup = BeautifulSoup(html_content, "html.parser")
 titre_h1 = soup.find("h1")
 text_titre = titre_h1.text
 
+liste_div_centre = soup.find_all("div", class_="centre")
+
+paragraphe_description = liste_div_centre[1].find("p", class_="description")
+text_description = paragraphe_description.text
+
 print ("Titre de la page:", text_titre)
+print ("La description:", text_description)
